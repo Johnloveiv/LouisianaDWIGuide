e5ba4f624c26f91be6fe487056e43b8216e84a25
Judson Mitchell <judsonmitchell@gmail.com>
2014-05-24 15:35:38 -0500

    Update js to do calculation, return results

--- BAC/BAC.js
+++ BAC/BAC.js
@@ -1,50 +1,62 @@
 //Define the result variable
-var BAC;
-
 function solveBAC(form) {
-   var ounces = form.ounces.value;
-   var percent = form.percent.value;
-   var weight = form.weight.value;
-   var hours = form.hours.value;
-   var gender = form.gender.option;
-   var result;
-   var message;
+    var ounces = form.ounces;
+    var percent = form.percent;
+    var weight = form.weight;
+    var hours = form.hours;
+    var gender = form.gender;
+    var result;
+    var message;
+    var BAC;
+    var data = [];
 
-   if (gender === 'true') {
+    if (gender === 'male') {
 
-       // This is the formula for males
-       BAC = (ounces * percent * 0.075 / weight) - (hours * 0.015);
+        // This is the formula for males
+        BAC = (ounces * percent * 0.075 / weight) - (hours * 0.015);
 
-       if (BAC < 0) {
-           message = 'You are not legally intoxicated.';
-           result = '-- neglible amount --';
-       } else if (BAC === 'NaN') {
-           message = 'Please try again.';
-       } else if (BAC > 0.08){
-           message = 'Under Louisiana Law You are Intoxicated.';
-       } else if (BAC < 0.08){
-           message = 'You are not legally intoxicated';
-       }
+        if (BAC < 0) {
+            data.message = 'You are not legally intoxicated.';
+        } else if (BAC === 'NaN') {
+            data.message = 'Please try again.';
+        } else if (BAC > 0.08){
+            data.message = 'Under Louisiana Law You are Intoxicated.';
+        } else if (BAC < 0.08){
+            data.message = 'You are not legally intoxicated';
+        }
 
-       form.message.value = message;
-       form.bacamount.value = result + ' %';
+    } else {
 
-   } else {
+        BAC = (ounces * percent * 0.075 / weight) - (hours * 0.017);
 
-       BAC = (ounces * percent * 0.075 / weight) - (hours * 0.017);
+        if (BAC < 0) {
+            data.message = 'You are not legally intoxicated.';
+        } else if (BAC === 'NaN'){
+            data.message = 'Please try again.';
+        } else if (BAC > 0.08){
+            data.message = 'Under Louisiana Law You are Intoxicated.';
+        } else if (BAC < 0.08){
+            data.message = 'You are not legally intoxicated';
+        }
 
-       if (BAC < 0) {
-           message = 'You are not legally intoxicated.';
-           result = '-- neglible amount --';
-       } else if (BAC === 'NaN'){
-           message = 'Please try again.';
-       } else if (BAC > 0.08){
-           message = 'Under Louisiana Law You are Intoxicated.';
-       } else if (BAC < 0.08){
-           message = 'You are not legally intoxicated';
-       }
+    }
 
-       form.message.value = message;
-       form.bacamount.value = result + ' %';
-   }
+    data.result = BAC;
+    return data;
 }
+
+//Submit the form
+$('form').submit(function (e){
+    e.preventDefault();
+    var vals = $('form').serializeArray();
+    var form = [];
+
+    //Loop through the submitted array and create an object with the data
+    vals.forEach(function (v){
+        form[v.name] = v.value;
+    });
+
+    var calculation = solveBAC(form);
+    $('#result').html(calculation.result + ' %');
+    $('#message').html(calculation.message);
+});
